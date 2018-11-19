a4b0e85a805267484c211e4f3190f04833f48cfb
Alex S <alex.sunga@fliptech.co>
2018-11-19 10:21:11 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -2,10 +2,17 @@
 # returns the phrase
 
 
+<<<<<<< HEAD
 def parrot(da = "Squawk!")
   
   puts da 
   
   return da 
+=======
+def parrot(da = "Squawk")
+  
+  parrot 
+  puts "#{da}"
+>>>>>>> f6cbae317a2190acaeb5f1630c7f8e22171bb5c8
  
 end 
